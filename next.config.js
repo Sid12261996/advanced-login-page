472221630b6be7d16487991f610f9f34cfd2ac11
Sidharth S <sidharthrkc@gmail.com>
2023-09-16 21:25:22 +0530

    resolving gitignore conflicts and remove static import as it will be done in next build

---
+++ next.config.js
@@ -0,0 +1,3 @@
+// Default Pages configuration for Next
+const nextConfig = {images: {unoptimized: true},experimental: {images: {unoptimized: true}},basePath: "/advanced-login-page", output: 'export',}
+module.exports = nextConfig
